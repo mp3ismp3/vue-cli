8aad04f0fe12946f0dbefc5f0ab9657eabd94fa7
Yeng Ping <peter2mary@gmail.com>
2020-10-06 16:53:54 +0800

    create message board

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <nav>
+      <div class="navigation__logo">
+        twooter
+      </div>
+      <div class="navigation__user">
+        {{ user.username}}
+      </div>
+    </nav>
+    <UserProfile/>
+  </div>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile";
+
+export default {
+  name: 'App',
+  components: { UserProfile },
+  data() {
+    return {
+      user: {
+        username: '_YengPingCheng'
+      }
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-color: #F3F5FA;
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 5%;
+    background-color: #FBFE37;
+    color: black;
+    .navigation__logo {
+      font-weight: bold;
+      font-size: 24px;
+    }
+  }
+}
+
+</style>
